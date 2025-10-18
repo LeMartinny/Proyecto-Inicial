3b1aaa38d4a2c8beae7328d2b6119043f2abdd00
LeMartinny <martingaticaschmidt@gmail.com>
2025-10-18 12:28:07 -0300

    hola

--- ProgresaTallaLibre/Core/urls.py
+++ ProgresaTallaLibre/Core/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from Core import views as core_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', core_views.home, name='home_core'),  # ruta para la página principal
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
--- ProgresaTallaLibre/Core/views.py
+++ ProgresaTallaLibre/Core/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+import time
 
 def home(request):
     return render(request, "core/home.html")
-    
+
+#def inscribirse(request):
